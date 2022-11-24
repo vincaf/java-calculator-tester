1f1f0637544fe1bdfe4bb759d101be0175e98a21
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-24 17:38:13 +0100

    feat: create Calculator class w/ methods

---
+++ maven-calculator/src/main/java/org/calculator/italy/Calculator.java
@@ -0,0 +1,20 @@
+package org.calculator.italy;
+
+public class Calculator {
+
+	public float add(float num1, float num2) {
+		return num1 + num2;
+	}
+
+	public float subtract(float num1, float num2) {
+		return num1 - num2;
+	}
+
+	public float divide(float num1, float num2) {
+		return num1 / num2;
+	}
+
+	public float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+}
