76c420c451740569deb50304ab47f949b55bf16d
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-24 17:39:14 +0100

    feat(Main): add new calculator
    print the methods of Calculator (add, substract, divide, multiply)

--- maven-calculator/src/main/java/org/calculator/italy/Main.java
+++ maven-calculator/src/main/java/org/calculator/italy/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		Calculator calc = new Calculator();
+		
+		System.out.println(calc.add(2, 1));
+		System.out.println(calc.subtract(2, 1));
+		System.out.println(calc.divide(4, 2));
+		System.out.println(calc.multiply(2, 2));
 	}
 
 }
